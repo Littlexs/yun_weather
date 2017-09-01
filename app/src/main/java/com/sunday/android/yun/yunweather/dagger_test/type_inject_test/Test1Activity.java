a519e0f244905313f2fd24a6c4f0ad986b1a74a5
xiaosheng <2300797269@qq.com>
2017-09-01 17:11:38 +0800

    add dagger test bean

---
+++ app/src/main/java/com/sunday/android/yun/yunweather/dagger_test/type_inject_test/Test1Activity.java
@@ -0,0 +1,32 @@
+package com.sunday.android.yun.yunweather.dagger_test.type_inject_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.sunday.android.yun.yunweather.R;
+import com.sunday.android.yun.yunweather.dagger_test.type_inject_test.test1.AClass;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/*
+* @Module @Provides @Component
+* */
+public class Test1Activity extends AppCompatActivity {
+
+    @BindView(R.id.tv1)
+    TextView textView;
+
+    AClass aClass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test1);
+        ButterKnife.bind(this);
+        textView.setText(aClass.getStr1());
+    }
+}
---
+++ app/src/main/java/com/sunday/android/yun/yunweather/dagger_test/type_inject_test/test1/CClass.java
@@ -0,0 +1,26 @@
+package com.sunday.android.yun.yunweather.dagger_test.type_inject_test.test1;
+
+/**
+ * Created by ASUS001 on 2017/9/1.
+ */
+
+public class CClass {
+    private String str1;
+    private String str2;
+
+    public String getStr1() {
+        return str1;
+    }
+
+    public void setStr1(String str1) {
+        this.str1 = str1;
+    }
+
+    public String getStr2() {
+        return str2;
+    }
+
+    public void setStr2(String str2) {
+        this.str2 = str2;
+    }
+}
